5445059a2afb1169239d593b18f2e15d8e3c1325
Owashim <aowashim@gmail.com>
2022-02-03 01:42:29 +0530

    Removed user id

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,33 @@ const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 
+// Check whether the user is available
+router.get('/check', async (req, res) => {
+  const myConnection = mysql.createConnection(process.env.DB)
+
+  myConnection.connect(err => {
+    if (err) return res.status(500).json({ msg: 'Server error.' })
+  })
+
+  myConnection.query(
+    `select userName from userinfo where userName=?`,
+    [`${req.query.un}`],
+    async (err, results) => {
+      if (err) {
+        res.status(500).json({ msg: 'Server error.' })
+      } else {
+        if (results.length) {
+          res.status(400).json({ msg: 'Username already taken.' })
+        } else {
+          res.status(200).json({ msg: 'Username available.' })
+        }
+      }
+    }
+  )
+
+  myConnection.end()
+})
+
 // register user (signup)
 router.post('/signup', async (req, res) => {
   const data = req.body
@@ -19,43 +46,30 @@ router.post('/signup', async (req, res) => {
   })
 
   myConnection.query(
-    `insert into userinfo(email, name, city, about, dp, pwrd) values('${data.email}', '${data.name}',
+    `insert into userinfo(userName, name, city, about, dp, pwrd) values('${data.userName}', '${data.name}',
       '${data.city}', '${data.about}', '${data.dp}', '${hashedPwd}')`,
     (err, results) => {
       if (err) {
         res.status(500).json({ msg: 'Server error.' })
       } else {
-        myConnection.query(
-          `select uid, pwrd from userinfo where email=?`,
-          [`${data.email}`],
-          async (err, results) => {
+        jwt.sign(
+          {
+            userName: data.userName,
+          },
+          process.env.JWT_SECRET,
+          (err, token) => {
             if (err) {
               res.status(500).json({ msg: 'Server error.' })
             } else {
-              if (results.length) {
-                jwt.sign(
-                  {
-                    uid: results[0].uid,
-                  },
-                  process.env.JWT_SECRET,
-                  (err, token) => {
-                    if (err) {
-                      res.status(500).json({ msg: 'Server error.' })
-                    } else {
-                      res.status(200).json({ token })
-                    }
-                  }
-                )
-              } else {
-                return res.status(400).json({ msg: 'Invalid credentials.' })
-              }
+              res.status(200).json({ token })
             }
-            myConnection.end()
           }
         )
       }
     }
   )
+
+  myConnection.end()
 })
 
 // Sign In and get token (signin)
@@ -69,8 +83,8 @@ router.post('/signin', async (req, res) => {
   })
 
   myConnection.query(
-    `select uid, pwrd from userinfo where email=?`,
-    [`${data.email}`],
+    `select pwrd from userinfo where userName=?`,
+    [`${data.userName}`],
     async (err, results) => {
       if (err) {
         res.status(500).json({ msg: 'Server error.' })
@@ -79,24 +93,24 @@ router.post('/signin', async (req, res) => {
           const isMatch = await bcrypt.compare(data.pwrd, results[0].pwrd)
 
           if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid credentials.' })
-          }
-
-          jwt.sign(
-            {
-              uid: results[0].uid,
-            },
-            process.env.JWT_SECRET,
-            (err, token) => {
-              if (err) {
-                res.status(500).json({ msg: 'Server error.' })
-              } else {
-                res.status(200).json({ token })
+            res.status(400).json({ msg: 'Invalid credentials.' })
+          } else {
+            jwt.sign(
+              {
+                userName: data.userName,
+              },
+              process.env.JWT_SECRET,
+              (err, token) => {
+                if (err) {
+                  res.status(500).json({ msg: 'Server error.' })
+                } else {
+                  res.status(200).json({ token })
+                }
               }
-            }
-          )
+            )
+          }
         } else {
-          return res.status(400).json({ msg: 'Invalid credentials.' })
+          res.status(400).json({ msg: 'Invalid credentials.' })
         }
       }
     }
